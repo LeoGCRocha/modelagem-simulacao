f806d3f5f5dee1d020aec383850501c617bf76ab
Leonardo Gideão Costa Rocha <leogcrocha@gmail.com>
2022-06-04 23:12:18 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,66 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Smart_Delay.cpp
- * Author: rlcancian
- * 
- * Created on 3 de Setembro de 2019, 18:34
- */
-
-#include "Smart_Delay.h"
-
-// you have to included need libs
-
-// GEnSyS Simulator
+#include "Water.h"
+// Geneysys Simulator
 #include "../../../../kernel/simulator/Simulator.h"
-
 // Model Components
 #include "../../../../plugins/components/Create.h"
 #include "../../../../plugins/components/Delay.h"
 #include "../../../../plugins/components/Dispose.h"
-#include "../../../../plugins/components/Process.h"
+#include "../../../../plugins/components/Seize.h"
+#include "../../../../plugins/components/Release.h"
+// Model data definitions
+#include "../../../../kernel/simulator/ModelDataManager.h"
+#include "../../../../plugins/data/Set.h"
 
-Smart_Delay::Smart_Delay() {
+// int Smart_AssignWriteSeizes::main(int argc, char** argv) {
+
+Water::Water() {
 }
 
-/**
- * This is the main function of the application. 
- * It instanciates the simulator, builds a simulation model and then simulate that model.
- */
-int Smart_Delay::main(int argc, char** argv) {
-	Simulator* genesys = new Simulator();
+int Water::main(int argc, char** argv) {
+    Simulator* genesys = new Simulator();
 	genesys->getTracer()->setTraceLevel(Util::TraceLevel::L4_warning);
 	this->setDefaultTraceHandlers(genesys->getTracer());
 	this->insertFakePluginsByHand(genesys);
-
-	// create model
-	Model* model = genesys->getModels()->newModel();
+    // create model
+    Model* model = genesys->getModels()->newModel();
 	PluginManager* plugins = genesys->getPlugins();
-	
-    Create* create = plugins->newInstance<Create>(model);
-	create->setEntityTypeName("Garrafa");
-	create->setTimeBetweenCreationsExpression("EXPO(1)");
-	create->setTimeUnit(Util::TimeUnit::second);
-	create->setEntitiesPerCreation(4);
-	
-	Resource* embaladora = plugins->newInstance<Resource>(model, "Embaladora");
-	embaladora->setCapacity(30);
+    // create
+	Create* create1 = plugins->newInstance<Create>(model);
+    create1->setEntityTypeName("Entity 1");
+    create1->setTimeBetweenCreationsExpression("EXPO(1)");
+    create1->setTimeUnit(Util::TimeUnit::second);
+    create1->setEntitiesPerCreation(4);
+    // SeizeDelayRelease
+    Resource* machine1 = plugins->newInstance<Resource>(model); // water bootle
+    machine1->setCapacity(30);
+    Queue* queueSeize1 = plugins->newInstance<Queue>(model);
+    queueSeize1->setOrderRule(Queue::OrderRule::FIFO);
+    Seize* seize1 = plugins->newInstance<Seize>(model);
+    seize1->getSeizeRequests()->insert(new SeizableItem(machine1));
+    seize1->setQueueableItem(new QueueableItem(model, "Seize_1.Queue"));
+    Delay* delay1 = plugins->newInstance<Delay>(model);
+    delay1->setDelayExpression("norm(5, 1)");
+    delay1->setDelayTimeUnit(Util::TimeUnit::second);
+    Release* release1 = plugins->newInstance<Release>(model);
+    release1->getReleaseRequests()->insert(new SeizableItem(machine1));
+    Dispose* dispose1 = plugins->newInstance<Dispose>(model);
+    create1->getConnections()->insert(seize1);
+    seize1->getConnections()->insert(release1);
+    release1->getConnections()->insert(delay1);
+    delay1->getConnections()->insert(dispose1);
 
-    Process* process = plugins->newInstance<Process>(model);
-	process->getSeizeRequests()->insert(new SeizableItem(embaladora));
-	process->setQueueableItem(new QueueableItem(plugins->newInstance<Queue>(model)));
-    process->setDelayTimeUnit(Util::TimeUnit::second);
-	process->setDelayExpression("NORM(5,1)");
-
-    Dispose* dispose = plugins->newInstance<Dispose>(model);
-
-    create->getConnections()->insert(process);
-	process->getConnections()->insert(dispose);
-
+    
     model->getSimulation()->setReplicationLengthTimeUnit(Util::TimeUnit::hour);
     model->getSimulation()->setReplicationReportBaseTimeUnit(Util::TimeUnit::hour);
     model->getSimulation()->setReplicationLength(24);
@@ -70,6 +59,6 @@ int Smart_Delay::main(int argc, char** argv) {
 	model->save("./models/ModelTest.gen");
 	model->getSimulation()->start();
 	genesys->~Simulator();
-	return 0;
-};
-
+    
+    return 0;
+}   
